0fefb2b1f18a74af3428ebd0b722936c6c5a4da3
akt.ryuu <liuyj@ciostc.com>
2020-08-09 11:42:14 +0900

    画面遷移用URL定数クラスを追加

---
+++ src/main/java/org/cios/employee/app/LoginController.java
@@ -0,0 +1,11 @@
+package org.cios.employee.app;
+
+import org.cios.employee.domain.common.UrlConsts;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(UrlConsts.LOGIN)
+@Controller
+public class LoginController {
+
+}
